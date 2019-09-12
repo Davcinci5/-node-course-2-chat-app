2c44fb2cd97f670864afc09b0587d12796e84e2e
Diego <diegoa.velascoc@gmail.com>
2019-09-12 15:26:46 -0700

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
 const path = require('path');
-
 const http = require('http');
-
 const express = require('express');
-
 const socketIO = require('socket.io');
-
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
-
 const port = process.env.PORT || 3000;
 
 var app = express();
-
 var server = http.createServer(app);
 // we get back is our WebSockets server
 var io = socketIO(server);
@@ -23,11 +18,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-       });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+
+    
+    // socket.emit('newMessage', {
+    //     from: 'Admin',
+    //     text: 'Welcome to the chat app',
+    //     createdAt: new Date().getTime()
+    //    });
+
     //new message
     // socket.emit('newMessage', {
     //     from: 'John',
@@ -41,11 +41,8 @@ io.on('connection', (socket) => {
 
 
     //io.emit emits an event to every single connection
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-           });
+    io.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
 
         // MADE USE OF BROADCAST
 
